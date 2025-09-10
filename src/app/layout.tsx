7fe3ec5443bf200cfeac3a8fba74c118c4adea76
majid <majeed.ghasemi@gmail.com>
2025-09-09 23:36:12 -0600

    fix exam teacher3

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -22,6 +22,7 @@ import "glightbox/dist/css/glightbox.css";
 import "plugins/scrollcue/scrollCue.css";
 // BOOTSTRAP & CUSTOM CSS
 import "assets/scss/style.scss";
+import Script from "next/script";
 
 const manrope = Manrope({
   subsets: ["latin"]
@@ -108,7 +109,16 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: PropsWithChildren) {
   return (
     <html lang="fa" dir="rtl">
+ 
       <body className={manrope.className}>
+        
+      <Script
+          id="goftino-widget"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `!function(){var i="QSa1es",a=window,d=document;function g(){var g=d.createElement("script"),s="https://www.goftino.com/widget/"+i,l=localStorage.getItem("goftino_"+i);g.async=!0,g.src=l?s+"?o="+l:s;d.getElementsByTagName("head")[0].appendChild(g);};"complete"===d.readyState?g():a.attachEvent?a.attachEvent("onload",g):a.addEventListener("load",g,!1);}();`,
+          }}
+        />
         <ThemeProvider>{children}</ThemeProvider>
 
         {/* USED FOR SCROLL ANIMATION */}
